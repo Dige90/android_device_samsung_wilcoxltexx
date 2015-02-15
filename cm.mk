b049c637c0c7f8b4a30e1b601935da5cc37071c8
Cristian <cristiandige90@gmail.com>
2015-02-15 05:11:14 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -6,10 +6,10 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/wilcoxltexx/full_wilcoxltexx.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wilcoxltexx TARGET_DEVICE=wilcoxlte
-
-PRODUCT_NAME := cm_wilcoxltexx
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wilcoxltexx TARGET_DEVICE=wilcoxlte
+
+PRODUCT_NAME := cm_wilcoxltexx
